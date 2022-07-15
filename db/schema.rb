6837fa8b39c5280d37e30d7e06e7071e038375df
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-15 18:26:46 +0500

    Added Child model & association [has_many & belongs_to] in child and parent

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_15_132000) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_132508) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "children", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.string "gender"
+    t.bigint "parent_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_children_on_parent_id"
+  end
 
   create_table "parents", force: :cascade do |t|
     t.string "name"
@@ -42,5 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_132000) do
     t.index ["pod_id"], name: "index_teachers_on_pod_id"
   end
 
+  add_foreign_key "children", "parents"
   add_foreign_key "teachers", "pods"
 end

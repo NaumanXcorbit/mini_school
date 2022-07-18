5cd9f634ff8cac782bd2368ecbec21e5f165d5cb
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-18 11:25:18 +0500

    Added Faker for Data Papulate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 2000.times do
+#   Teacher.create(
+#     name: Faker::Artist.name,
+#     email: Faker::Internet.email,
+#     address: Faker::Address.full_address,
+#     phone: Faker::PhoneNumber.cell_phone,
+#     pod_id: Pod.first.id
+#   )
+# end

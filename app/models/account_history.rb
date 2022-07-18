6174c62eb2aeb0924dd66c9d11f06b57f42b9eb5
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2022-07-18 19:42:59 +0500

    scopes define for accounthistory status

--- app/models/account_history.rb
+++ app/models/account_history.rb
@@ -1,3 +1,6 @@
 class AccountHistory < ApplicationRecord
   belongs_to :account
+  scope :pending, -> { where(account_status: "Pending") }
+  scope :active, -> { where(account_status: "Active") }
+  scope :idle, -> { where(account_status: "Idle") }
 end
--- app/models/child.rb
+++ app/models/child.rb
@@ -3,4 +3,5 @@ class Child < ApplicationRecord
   has_many :child_pods
   has_many :pods, through: :child_pods
   has_and_belongs_to_many :subjects
+  scope :baby, -> { where(date_of_birth: Time.now.midnight - 4.hours) }
 end
